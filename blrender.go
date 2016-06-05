efc9b13c8fa0c646806e8981f37698a6697fff2c
kimhanwoong <khw7096@gmail.com>
2016-06-05 22:23:30 +0900

    버그수정. 1차 작동되도록 수정함

--- blrender.go
+++ blrender.go
@@ -3,17 +3,32 @@ package main
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
+	"log"
+	"strings"
+	"fmt"
 	)
-
-func file2renderpath(filename string) string {
+func path2renderpath(path string) string {
 	//test.blend -> ./test/test.####.exr"
-	filename := strings.Split(filename, ".blend")[0]
+	_, file := filepath.Split(path)
+	ext := filepath.Ext(file)
+	if ext != ".blend" {
+		fmt.Println("블랜더파일이 아닙니다.")
+		os.Exit(1)
+	}
+	filename := strings.Split(file, ext)[0]
+	return fmt.Sprintf("./%s/%s.####.exr", filename, filename)
 }
-
 func main() {
+	cwdpath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
 	osx := "/Applications/Blender/blender.app/Contents/MacOS/blender"
+
 	if _, err := os.Stat(osx); err == nil {
-		cmd := exec.Command(osx, "-b", os.Args[1], "-o", "./test/test.####.exr", "-F", "EXR", "-a")
+		bfile := cwdpath + "/" + os.Args[1]
+		cmd := exec.Command(osx, "-b", bfile, "-o", path2renderpath(bfile), "-F", "EXR", "-a")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
