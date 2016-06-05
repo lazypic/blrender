bd3daab8f74b21b7c852f68c4a87ac309ec6a8ab
kim han woong <khw7096@gmail.com>
2016-06-05 12:45:08 +0800

    add file

--- blrender.go
+++ blrender.go
@@ -1,6 +1,21 @@
 package main
 
-import ()
+import (
+	"os"
+	"os/exec"
+	)
+
+func file2renderpath(filename string) string {
+	//test.blend -> ./test/test.####.exr"
+	filename := strings.Split(filename, ".blend")[0]
+}
 
 func main() {
+	osx := "/Applications/Blender/blender.app/Contents/MacOS/blender"
+	if _, err := os.Stat(osx); err == nil {
+		cmd := exec.Command(osx, "-b", os.Args[1], "-o", "./test/test.####.exr", "-F", "EXR", "-a")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
 }
