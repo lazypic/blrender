80c974ba06da6f956b5d7bbc2cec8d0dc45fc5d4
kim han woong <khw7096@gmail.com>
2017-10-01 14:27:32 +0900

    예제추가

--- blrender.go
+++ blrender.go
@@ -6,13 +6,14 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 )
-
 const (
 	MAC = "/Applications/Blender/blender.app/Contents/MacOS/blender"
+	//LIN = ""
+	//WIN
 )
 
+// renderPath 함수는 블랜더파일을 받아서 렌더링에 필요한 렌더경로를 반환한다.
 func renderPath(blenderPath string) string {
 	ext := filepath.Ext(blenderPath)
 	if ext != ".blend" {
@@ -20,27 +21,29 @@ func renderPath(blenderPath string) string {
 		os.Exit(1)
 	}
 	filename := filepath.Base(blenderPath)
-	filename := filename[:len(filename)-len(ext)]
+	filename = filename[:len(filename)-len(ext)]
 	return fmt.Sprintf("./%s/%s.####.exr", filename, filename)
 }
 
 func main() {
+	_, err := os.Stat(MAC)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s 위치에 blender가 없습니다.\n", MAC)
+		fmt.Fprintln(os.Stderr, "블렌더설치가 필요합니다.")
+		os.Exit(1)
+	}
+	if len(os.Args) != 2 {
+		fmt.Println("How to use :")
+		fmt.Println("$ blrender <blender filename>")
+		os.Exit(1)
+	}
 	cwdpath, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(os.Args) != 2 {
-		fmt.Println("How to use :\n $ blrender <blender filename>")
-		os.Exit(1)
-	}
-
-	_, err := os.Stat(MAC)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "블렌더설치가 필요함.")
-		os.Exit(1)
-	}
-	bfile := cwdpath + "/" + os.Args[1]
-	cmd := exec.Command(MAC, "-b", bfile, "-o", renderPath(blenderPath), "-F", "EXR", "-a")
+	blenderPath := cwdpath + "/" + filepath.Base(os.Args[1])
+	// blender파일에서 설정된 프레임값으로 exr렌더링 한다.
+	cmd := exec.Command(MAC, "-b", blenderPath, "-o", renderPath(blenderPath), "-F", "EXR", "-a")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
---
+++ test/test.sh
@@ -0,0 +1,1 @@
+blrender rendertest.blend
